d219648ce9c2b29d1f50d9ef76c87beffb6d317f
jhjwlee <jh.jw.lee@gmail.com>
2025-06-24 06:37:05 +0900

    Update setup.ps1

--- Allfiles/labs/08/setup.ps1
+++ Allfiles/labs/08/setup.ps1
@@ -25,18 +25,40 @@ $dataLakeAccountName = Read-Host "Enter the existing Data Lake Storage Account N
 $sqlDatabaseName = Read-Host "Enter the existing Dedicated SQL Pool Name"
 $sqlUser = "SQLUser"
 
+# 비밀번호 입력 (일반 문자열, 복잡성 검사 포함)
+$complexPassword = 0
+while ($complexPassword -ne 1) {
+    $sqlPassword = Read-Host "Enter a password for '$sqlUser' (min 8 chars, 1 upper, 1 lower, 1 digit, 1 special)"
+
+    if(($sqlPassword -cmatch '[a-z]') -and ($sqlPassword -cmatch '[A-Z]') -and ($sqlPassword -match '\d') -and ($sqlPassword.Length -ge 8) -and ($sqlPassword -match '[!@#%^\&\$]')) {
+        $complexPassword = 1
+        Write-Output "Password accepted. Make sure you remember this!"
+    } else {
+        Write-Output "Password does not meet complexity requirements. Please try again."
+    }
+}
+
+# 권한 부여
+Write-Host "Granting permissions on the $dataLakeAccountName storage account..."
+$subscriptionId = (Get-AzContext).Subscription.Id
+$userName = ((az ad signed-in-user show) | ConvertFrom-Json).UserPrincipalName
+$spId = (Get-AzADServicePrincipal -DisplayName $synapseWorkspace).Id
+
+New-AzRoleAssignment -ObjectId $spId -RoleDefinitionName "Storage Blob Data Owner" -Scope "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$dataLakeAccountName" -ErrorAction SilentlyContinue
+New-AzRoleAssignment -SignInName $userName -RoleDefinitionName "Storage Blob Data Owner" -Scope "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$dataLakeAccountName" -ErrorAction SilentlyContinue
+
 # 데이터 로드
 Write-Host "Loading data to $sqlDatabaseName ..."
 Get-ChildItem "./data/*.txt" -File | ForEach-Object {
     $file = $_.FullName
     $table = $_.Name.Replace(".txt", "")
     Write-Host "Importing $file to table $table..."
-    bcp dbo.$table in $file -S "$synapseWorkspace.sql.azuresynapse.net" -U $sqlUser -P $plainPassword -d $sqlDatabaseName -f $file.Replace("txt", "fmt") -q -k -E -b 5000
+    bcp dbo.$table in $file -S "$synapseWorkspace.sql.azuresynapse.net" -U $sqlUser -P $sqlPassword -d $sqlDatabaseName -f $file.Replace("txt", "fmt") -q -k -E -b 5000
 }
 
 # SQL Pool 일시 중지
-Write-Host "Pausing the $sqlDatabaseName SQL Pool..."
-Suspend-AzSynapseSqlPool -WorkspaceName $synapseWorkspace -Name $sqlDatabaseName -AsJob
+#Write-Host "Pausing the $sqlDatabaseName SQL Pool..."
+#Suspend-AzSynapseSqlPool -WorkspaceName $synapseWorkspace -Name $sqlDatabaseName -AsJob
 
 # 스크립트 업로드
 $solutionScriptPath = "Solution.sql"
